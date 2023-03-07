decedced0f742f885a996fb122289bdbadf310df
ClearCB <abelcasasccb@gmail.com>
2023-03-07 16:21:20 +0100

    style(RomanNumber): remove whitepsaces

--- romannumerals/src/main/java/edu/craptocraft/romannumerals/RomanNumber.java
+++ romannumerals/src/main/java/edu/craptocraft/romannumerals/RomanNumber.java
@@ -1,6 +1,15 @@
 package edu.craptocraft.romannumerals;
 
 public class RomanNumber {
-    
+    private String number;
+
+    public RomanNumber(String numberSymbols) {
+        this.number = numberSymbols;
+    }
+
+    @Override
+    public String toString() {
+        return number;
+    }
 
 }
