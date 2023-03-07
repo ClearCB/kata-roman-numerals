64a421b92b70a0924f1f9ff709832129efe3f5b4
ClearCB <abelcasasccb@gmail.com>
2023-03-07 16:25:48 +0100

    build(ttest): add new file

--- romannumerals/src/test/java/edu/craptocraft/romannumerals/RomanNumberTest.java
+++ romannumerals/src/test/java/edu/craptocraft/romannumerals/RomanNumberTest.java
@@ -19,27 +19,6 @@ public class RomanNumberTest {
         romanTwelve = new RomanNumber("XII");
     }
 
-    // Enum correct behaviour
-    @Test
-    public void symbolsTest() {
-
-        assertEquals(1, Symbols.I.getValue());
-        assertEquals(5, Symbols.V.getValue());
-        assertEquals(10, Symbols.X.getValue());
-        assertEquals(50, Symbols.L.getValue());
-        assertEquals(100, Symbols.C.getValue());
-        assertEquals(500, Symbols.D.getValue());
-        assertEquals(1000, Symbols.M.getValue());
-        assertEquals(4, Symbols.IV.getValue());
-        assertEquals(9, Symbols.IX.getValue());
-        assertEquals(40, Symbols.XL.getValue());
-        assertEquals(90, Symbols.XC.getValue());
-        assertEquals(400, Symbols.CD.getValue());
-        assertEquals(900, Symbols.CM.getValue());
-
-        assertEquals("I", Symbols.I.name());
-    }
-
     @Test
     public void constructoTest(){
 
